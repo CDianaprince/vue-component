37abbae73422abe28a94a49577436baf0774d0ba
unknown <dcarol.prince@gmail.com>
2023-02-27 10:55:56 -0600

    finalizacioni componentes dinamicos

--- src/componentes/Bookinfo.vue
+++ src/componentes/Bookinfo.vue
@@ -1,38 +1,70 @@
 <script>
 export default {
+  /*props : ["id","title","author","releaseDate","isLiked"],*/
+  //Objeto que valide los props
+  props:{
+    id: {
+      type: String,
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    releaseDate: {
+      type: String,
+      required: true,
+    },
+    isLiked: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: function(value) {
+        return value === "1" || value === "0";
+      },
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
-      Books:{
-          id: 1,
-          title: "Narnia",
-          author: "C.S Lewis",
-          releaseDate: "1999-12-01",
-      },
+      bookIsLiked: this.isLiked,
     };
   },
-  methods:{
+  methods: {
     toogleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
+    toogleFavorite() { 
+      if (this.bookIsLiked === "1"){
+      this.bookIsLiked ="0";
+    } else {
+      this.bookIsLiked = "1";
+      } 
+    },
   },
 };
-
 </script>
 
 <template>
+  <li>
+    <h2>
+      {{ title }} {{ bookIsLiked === "1" ? "(Liked)" : ""}}
+    </h2>
 
-<li>
-  <h2>
-  {{  Books.title }}
-  </h2>
+    <button type="button" @click="toogleDetails">
+      {{ detailsAreVisible ? "hide" : "show" }}
+    </button>
 
-  <button type="button" @click="toogleDetails">{{ detailsAreVisible ? "hide" : "show" }}</button>
+    <button type="button" @click="toogleFavorite">Toggle Favorite</button>
 
-  <ul v-if="detailsAreVisible
-  ">
-    <li><strong>Author: </strong> {{ Books.author }}</li>
-    <li><strong>Release Date:</strong> {{ Books.releaseDate }}</li>
-  </ul>
-</li>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Author: </strong> {{ author }}</li>
+      <li><strong>Release Date:</strong> {{ releaseDate }}</li>
+    </ul>
+  </li>
 </template>
